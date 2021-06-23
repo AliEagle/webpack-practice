4c4fa82836366870fede0700740f96cf4e79eef7
alicp <alicruzaguila@gmail.com>
2021-06-23 18:19:01 -0500

    WP loaders and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
@@ -10,4 +13,39 @@ module.exports = {
 	resolve: {
 		extensions: ['.js'],
 	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.css|.styl$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader'],
+			},
+			{
+				test: /\.png/,
+				type: 'asset/resource',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: true,
+			template: './public/index.html',
+			filename: 'index.html',
+		}),
+		new MiniCssExtractPlugin(),
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, 'src', 'assets/images'),
+					to: 'assets/images',
+				},
+			],
+		}),
+	],
 };
